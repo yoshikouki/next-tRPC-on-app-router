7148bcd77bd99170b959fe538985dc8e25c9b4d4
yoshikouki <yoshikouki@gmail.com>
2023-06-02 23:23:39 +0900

    WIP

---
+++ src/app/server/routers/_app.ts
@@ -0,0 +1,34 @@
+import { publicProcedure, router } from "../trpc";
+
+import { z } from "zod";
+
+export const createPost = publicProcedure
+  .input(
+    z.object({
+      title: z.string(),
+      content: z.string(),
+    })
+  )
+  .mutation(async (opts) => {
+    return {
+      id: "1",
+      ...opts.input,
+    };
+  });
+
+export const appRouter = router({
+  greeting: publicProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query(async (opts) => {
+      console.log("request from", opts.ctx.headers?.["x-trpc-source"]);
+      return `hello ${opts.input.text}`;
+    }),
+
+  createPost,
+});
+
+export type AppRouter = typeof appRouter;
